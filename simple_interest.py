84f688cf7701ba1749dfc36ee4787fcb9af52440
Atish2708 <Atish.9377@gmail.com>
2024-06-28 22:07:50 +0530

    files added

---
+++ simple_interest.py
@@ -0,0 +1,21 @@
+# This application calculate simple interest
+
+def simple_interest(P,N,R):
+       I = (P*N*R)/100
+       A = P + I
+       return I, A
+
+# Take Principal Period and Rate of Intrest as input from user
+P= float(input("Please enter Principal in INR : "))
+N = float(input("Please enter years : "))
+R = float(input("Please enter Rate of Intrest in %p.a."))
+
+# Print the intrest and amout
+I, A = simple_interest(P, N, R)
+print(f'The Simple Intrest is : {I:.2f} INR')
+print(f'The Total amount is : {A:.2f} INR')
+
+
+
+
+    
